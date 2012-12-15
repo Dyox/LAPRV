747409076e975d89120a52c5813fb5829794fb0d
Dyox <carlosrok2003@gmail.com>
2012-12-15 16:21:39 +0000

    projecto para executar prolog atraves de c++
    compila sem erros
    
    to do: testar executar simples .pl
    
    Signed-off-by: Dyox <carlosrok2003@gmail.com>

---
+++ C++/c++Prlog/c++Prlog/Stdafx.h
@@ -0,0 +1,35 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+//
+//#if !defined(AFX_STDAFX_H__D183AC51_3813_4076_A397_8808C0E87D1F__INCLUDED_)
+//#define AFX_STDAFX_H__D183AC51_3813_4076_A397_8808C0E87D1F__INCLUDED_
+//
+//#if _MSC_VER > 1000
+//#pragma once
+//#endif // _MSC_VER > 1000
+//
+//#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+////
+//#ifndef _AFX_NO_AFXCMN_SUPPORT
+//#include <afxcmn.h>			// MFC support for Windows Common Controls
+//#endif // _AFX_NO_AFXCMN_SUPPORT
+//
+//#endif // !defined(AFX_STDAFX_H__D183AC51_3813_4076_A397_8808C0E87D1F__INCLUDED_)
+//
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+
+#include <atlbase.h>
+#include <atlstr.h>
+
+// TODO: reference additional headers your program requires here

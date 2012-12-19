ae113182389110e76a0ef3c2a1bd27a8ab16429b
raro <raro.bruno@gmail.com>
2012-12-19 01:00:53 +0000

    mais alguns predicados
    Signed-off-by: raro <raro.bruno@gmail.com>

--- Prolog/lapr.pl
+++ Prolog/lapr.pl
@@ -31,11 +31,11 @@ go(X,Dest,[X|LA],L).
 amigos(X,Y):-ramo(X,Y,_);ramo(Y,X,_).
 
 /*lista uma lista com os amigos de um user*/
-listar(X):-findall(L,listar_amigos(X,L),Lista),
+/*listar(X):-findall(L,listar_amigos(X,L),Lista),
 		write(Lista),nl.
 
 listar_amigos(X,L):-listar_amigos(X,[],L).
-listar_amigos(X,L,[Y|L]):-ramo(X,Y,_).
+listar_amigos(X,L,[Y|L]):-ramo(X,Y,_).*/
 
 /*listar todos os amigos de uma tag*/
 listar_amigos_tag(X,Tag,L):-listar_amigos_tag(X,Tag,[],L).
@@ -44,13 +44,55 @@ listar_amigos_tag(X,Tag,L,[Y|L]):-ramo(X,Y,Tag,_).
 /*listar_tags(X,L):-listar_tags(X,[],L).
 listar_tags(X,L,[Y|L]):-no(X,Y,_,_).*/
 
+/*
 listar_tags(X,L):-listar_tags(X,[],L).
 listar_tags(X,L,[H|L]):-no(X,[H|T],_,_),compara(X,H,L).
 
 compara(X,H,L):-findall(L,listar_amigos(X,L),Lista),
 		no([[A|Lista]|L1],L2,_,_),write(L1).
+*/
 
 
+/*procurar users com uma dada tag */
+listar_users_tag(Tag,L):-listar_users_tag(Tag,[],L).
+listar_users_tag(Tag,L,[Y|L]):-no(Y,Lista,_,_),member(Tag,Lista).
+
+/*
+conta_tag(Tag,L):-conta_tag(Tag,[],L).
+conta_tag(Tag,L,[Y|L]):-no(_,Lista,_,_),member(Tag,Lista),Y is + 1, write(Y).
+*/
+
+/*verifica se um user ja tem aquela tag*/
+/*recebe o user e a tag, retorna yes or no*/
+verifica_tag(X,Tag):-no(X,L,_,_),member(Tag,L).
+
+
+/**/
+listar(X):-findall(L,listar_amigos(X,L),Lista),
+		write(Lista),nl.
+
+listar_amigos(X,L):-listar_amigos(X,[],L).
+listar_amigos(X,L,[Y|L]):-ramo(X,Y,_,_).
+
+/*conta os amigos de um user*/
+conta_amigos(X):-findall(L,listar_amigos(X,L),Lista),
+			conta_elem(Lista,Y),
+			write(Y),nl.
+
+/*lista os amigos dos amigos de um user*/
+/*listar_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),
+*/		
+
+
+		
+		/*listar(X),
+		listar_amigos_amigos(L).*/
+
+
+
+/*dada uma lista conta os seus elementos*/
+conta_elem([],0).
+conta_elem([_|T],N):-len(T,M),N is M+1.
 
 
 

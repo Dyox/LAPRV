8b12e018aa755c977425c7e0b3285fe87baba9a2
raro <raro.bruno@gmail.com>
2012-12-21 00:12:10 +0000

    lista amigos dos amigos
    limpa listas de listas
    limpa repetidos
    ...
    
    Signed-off-by: raro <raro.bruno@gmail.com>

--- Prolog/lapr.pl
+++ Prolog/lapr.pl
@@ -10,14 +10,25 @@ no(f,['c#'],90,95).
 
 % ramo(No1_ID,No2_ID,Tag,Força)
 ramo(a,b,[namorada],1).
+ramo(b,a,[namorada],1).
 ramo(a,c,[amiga],2).
+ramo(c,a,[amiga],2).
 ramo(a,d,[amiga],1).
+ramo(d,a,[amiga],1).
 ramo(a,e,[amiga],2).
+ramo(e,a,[amiga],2).
 ramo(b,e,[amiga],1).
+ramo(e,b,[amiga],1).
 ramo(d,e,[amiga],1).
+ramo(e,d,[amiga],1).
 ramo(d,f,[amiga,irma],1).
+ramo(f,d,[amiga,irma],1).
 ramo(c,d,[],3).
+ramo(d,c,[],3).
+ramo(e,h,[],3).
+ramo(h,e,[],3).
 
+/*ver acertz*/
 semantic_eq([['c#','csharp'],['vb','visual basic']]).
 
 /*todos os caminhos entre dois pontos*/
@@ -65,18 +76,20 @@ conta_tag(Tag,L,[Y|L]):-no(_,Lista,_,_),member(Tag,Lista),Y is + 1, write(Y).
 
 /*verifica se um user ja tem aquela tag*/
 /*recebe o user e a tag, retorna yes or no*/
+
 verifica_tag(X,Tag):-no(X,L,_,_),member(Tag,L).
 
 
 
 /**/
 listar_am(X):-findall(L,listar_amigos(X,L),Lista),
-		write(Lista),nl,
-		constroi_lista(Lista,[],L).
+		write(Lista),nl.
 
-listar_amigos(X,L):-listar_amigos(X,[],L).
+
+listar_amigos(X,Y):-ramo(X,Y,_,_).
+/*listar_amigos(X,L):-listar_amigos(X,[],L).
 listar_amigos(X,L,[Y|L]):-ramo(X,Y,_,_).
-
+*/
 
 
 
@@ -87,25 +100,88 @@ conta_amigos(X):-findall(L,listar_amigos(X,L),Lista),
 			conta_elem(Lista,Y),
 			write(Y),nl.
 
+conta_amigos_3nivel(X):-findall(X,listar_amigos_amigos(Lista,X),Lista),
+			conta_elem(Lista,Y),
+			write(Y),nl.
+
+
 
 
 
 
 /*lista os amigos dos amigos de um user*/
-listar_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),
+/*p([[X|_]|L1]):-listar_amigos2(X,L),p(L1).*/
+/*listar_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),
 				p(Lista).
-p([[X|_]|L1]):-listar_amigos2(X,L),p(L1).
+
+p([X|L1]):-listar_amigos(X|Y),p(L1).*/
+
+/**a funcionar listar_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),
+				findall(Y,listar_amigos_amigos(Lista,Y),Li),
+				write(Li),nl.
+
+listar_amigos_amigos([X|L],Y):-ramo(X,Y,_,_).*/
+
+/*listar_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),corre(Lista),write(Li).
+				corre([]).
+				corre([A|List]):-
+					findall(Y,listar_amigos_amigos(A,Y),Li),write(Li),nl,
+				corre(List).
+
+listar_amigos_amigos(X,Y):-ramo(X,Y,_,_).*/
+
+listar_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),corre(Lista,F),write(F).
+				corre([],[]).
+				corre([A|List],[Li|Final]):-
+					findall(Y,listar_amigos_amigos(A,Y),Li),
+				corre(List,Final).
+
+listar_amigos_amigos(X,Y):-ramo(X,Y,_,_).
+
+limpar([],[]).
+limpar([[X|A]|L],[X|Final]):-(A\==[]->(limpar([[A]|L],Final);limpar([L],Final))).
+
+function([],[]):- !.
+function([H|T],[H|R]):- \+ is_list(H), !, function(T,R).
+function([H|T],L):- function(H,L1), function(T,L2), append(L1,L2,L).
+
+is_list(X) :-
+        var(X), !,
+        fail.
+is_list([]).
+is_list([_|T]) :-
+        is_list(T).
+
+/*--*/
+member2(X,[X|_]):- !.
+member2(X,[_|T]) :- member2(X,T).
+
+set([],[]).
+set([H|T],[H|Out]) :-
+   not(member2(H,T)),
+   !,
+set(T,Out).
+set([H|T],Out) :-
+   member2(H,T),
+   set(T,Out).
+
+
 
 /*listar_amigos2(X,L),p(L1).*/
-
-listar_amigos2(X,L):-listar_amigos2(X,[],L).
+/*listar_amigos2(X,L):-listar_amigos2(X,[],L).
 listar_amigos2(X,L,[Y|L]):-ramo(X,Y,_,_),write(Y),nl.
 
 listar_am_am(X):-findall(_,listar_amigos_amigos(X),Lista),
 		nl,write(Lista),nl.
 
+*/
 
+listar_amigos_amigos_amigos(X):-findall(L,listar_amigos(X,L),Lista),
+				findall(Y,listar_amigos_amigos(Lista,Y),Li),
+				findall(Y,listar_amigos_amigos_amigos(Li,Y),Lis),
+				write(Lis),nl.
 
+listar_amigos_amigos_amigos([X|L],Y):-ramo(X,Y,_,_).
 
 
 
@@ -122,4 +198,23 @@ amigos_tags_iguais(U,X,L,[Y|L]):-no(Y,La,_,_),
 					no(U,[Tag|L],_,_),
 					
 					member(Tag,La),write(Tag).
-					
+
+
+
+/*cam_min(a,b,L) retorna o caminho minimo entre dois pontos*/
+caminhos(X,Y,P):-todos_caminhos(X,Y,[Y],P).
+todos_caminhos(X,Y,L,[X|L]):-ramo(X,Y,_,_).
+todos_caminhos(X,Y,L,P):-ramo(Z,Y,_,_),Z\==X,(not member(Z,L)),
+				todos_caminhos(X,Z,[Z|L],P).
+
+lstcaminhos(X,Y,L):-findall(P,caminhos(X,Y,P),L).
+
+menor([X],X).
+menor([X|T],X):-menor(T,L),
+				length(X,N),
+				length(L,M),
+				N =< M,!.
+menor([X|T],L):-menor(T,L).
+cam_min(X,Y,P):-findall(P,caminhos(X,Y,P),L),	
+			menor(L,P),write(P);write('Nenhum caminho...').
+/*---------------------*/					

e547ca72d9a1dd67cd250de1a6c91d0f27d3a535
Dyox2 <carlosrok2003@hotmail.com>
2012-12-26 11:07:02 +0000

    obter os nos e ramos atraves de ficheiro
    obter o prolog do comando atraves de ficheiro

--- C#/ExemploProf/ExemploProf/Program.cs
+++ C#/ExemploProf/ExemploProf/Program.cs
@@ -27,21 +27,24 @@ namespace ExemploProf
             //                    "cam_min(X,Y,P):-findall(P,caminhos(X,Y,P),L),menor(L,P),write(P),nl.",
             //                    "run:- tell('user_comando.txt'),cam_min(f,d,P),told,halt.",
             //                     ":-run."};
-            string lines =  "no(a,[musica,rock],45,95). no(b,[musica,rock],90,95). no(c,[musica],45,95). no(d,[],90,95). no(e,[],45,95). no(f,['c#'],90,95). "+ 
-                                "ramo(a,b,[namorada],1). ramo(a,c,[amiga],2). ramo(a,d,[amiga],1). ramo(a,e,[amiga],2). ramo(b,e,[amiga],1). ramo(d,e,[amiga],1). ramo(d,f,[amiga,irma],1). ramo(c,d,[],3). "+
-                                "caminhos(X,Y,P):-todos_caminhos(X,Y,[Y],P). "+
-                                "todos_caminhos(X,Y,L,[X|L]):-ramo(X,Y,_,_). "+
-                                "todos_caminhos(X,Y,L,P):-ramo(Z,Y,_,_),Z\\==X, (not member(Z,L)), todos_caminhos(X,Z,[Z|L],P). "+
-                                "lstcaminhos(X,Y,L):-findall(P,caminhos(X,Y,P),L). "+
-                                "menor([X],X). "+
-                                "menor([X|T],X):-menor(T,L) ,length(X,N),length(L,M),N =< M,!. "+
-                                "menor([X|T],L):-menor(T,L). "+
-                                "cam_min(X,Y,P):-findall(P,caminhos(X,Y,P),L),menor(L,P),write(P);write('Nenhum caminho...'). " +
-                                "run:- tell('user_comando.txt'),cam_min(a,b,P),told,halt. "+
-                                 ":-run. \n";
-            
-            PrologExec p = new PrologExec(lines, "user", "add","user_comando");
-            string res = p.executaComandoProlog();
+            //string lines =  "no(a,[musica,rock],45,95). no(b,[musica,rock],90,95). no(c,[musica],45,95). no(d,[],90,95). no(e,[],45,95). no(f,['c#'],90,95). "+ 
+            //                    "ramo(a,b,[namorada],1). ramo(a,c,[amiga],2). ramo(a,d,[amiga],1). ramo(a,e,[amiga],2). ramo(b,e,[amiga],1). ramo(d,e,[amiga],1). ramo(d,f,[amiga,irma],1). ramo(c,d,[],3). "+
+            //                    "caminhos(X,Y,P):-todos_caminhos(X,Y,[Y],P). "+
+            //                    "todos_caminhos(X,Y,L,[X|L]):-ramo(X,Y,_,_). "+
+            //                    "todos_caminhos(X,Y,L,P):-ramo(Z,Y,_,_),Z\\==X, (not member(Z,L)), todos_caminhos(X,Z,[Z|L],P). "+
+            //                    "lstcaminhos(X,Y,L):-findall(P,caminhos(X,Y,P),L). "+
+            //                    "menor([X],X). "+
+            //                    "menor([X|T],X):-menor(T,L) ,length(X,N),length(L,M),N =< M,!. "+
+            //                    "menor([X|T],L):-menor(T,L). "+
+            //                    "cam_min(X,Y,P):-findall(P,caminhos(X,Y,P),L),menor(L,P),write(P);write('Nenhum caminho...'). " +
+            //                    "run:- tell('user_comando.txt'),cam_min(a,c,P),told,halt. "+
+            //                     ":-run. \n";
+
+
+
+            //menorCaminho = cam_min(a,c,P)
+            PrologExec p = new PrologExec("user", "menorCaminho" );
+            string res = p.executaComandoProlog("cam_min(a,b,P)");
             Console.Write(res);
             Console.Read();
         }
--- C#/ExemploProf/ExemploProf/PrologExec.cs
+++ C#/ExemploProf/ExemploProf/PrologExec.cs
@@ -12,6 +12,7 @@ namespace ExemploProf
         string _User;
         string _Comando;
         string _NomeFich;
+        //string _Target;
 
         private PrologExec()
         {
@@ -22,16 +23,15 @@ namespace ExemploProf
             this._Lines = lines;
             this._User = user;
             this._Comando= comando;
-            //NomeFich=Comando+"_"+User;
-            this._NomeFich = nomeFich;
+            NomeFich=Comando+"_"+User;
+            //this._NomeFich = nomeFich;
         }
 
-        public PrologExec(string lines, string user, string comando)
+        public PrologExec(string user, string comando)
         {
-            this._Lines = lines;
             this._User = user;
             this._Comando = comando;
-            //NomeFich=Comando+"_"+User;
+            NomeFich = Comando + "_" + User;
         }
         
         public string Lines
@@ -58,11 +58,11 @@ namespace ExemploProf
             set { _Comando = value; }
         }
 
-        private Boolean escrverPL()
+        private Boolean escrverPL(string target)
         {
+            obterLines(target);
             if (Lines == null)
                 return false;
-            
             try
             {
                 System.IO.File.WriteAllText(@"C:\\WIN-PROLOG 4800\"+NomeFich+".pl", Lines);
@@ -72,6 +72,25 @@ namespace ExemploProf
                 return false;
             }
             return true;
+        }
+
+        private void obterLines(string target)
+        {
+            string nos = resultadoFicheiro("readNos");
+            string ramos = resultadoFicheiro("readRamos");
+            string conteudo = null;
+            switch (Comando)
+            {
+                case "menorCaminho":
+                    conteudo = resultadoFicheiro("menorCaminho");
+                    break;
+            }
+            string fim ="run:- tell('" + NomeFich + ".txt'),_target_,told,halt."+ "\n" +
+                                 ":-run. \n";
+            fim = fim.Replace("_target_",target);
+            if (nos == null && ramos == null && conteudo == null)
+                Lines = null;
+            Lines = nos + "\n" + ramos + "\n" + conteudo + "\n" + fim;
         }
 
         private Boolean executaProlog()
@@ -100,11 +119,11 @@ namespace ExemploProf
             }
         }
 
-        private string resultadoProlog()
+        private string resultadoFicheiro(string nomeFicheiro)
         {
             try
             {
-                string text = System.IO.File.ReadAllText(@"C:\\WIN-PROLOG 4800\" + NomeFich + ".txt");
+                string text = System.IO.File.ReadAllText(@"C:\\WIN-PROLOG 4800\" + nomeFicheiro + ".txt");
                 return text;
             }
             catch (System.IO.IOException e)
@@ -151,16 +170,15 @@ namespace ExemploProf
             return true;
         }
 
-        public string executaComandoProlog()
+        public string executaComandoProlog(string target)
         {
-        //Lines=Lines.Replace("user_comando",NomeFich);
-            Boolean pl =escrverPL();
+            Boolean pl =escrverPL(target);
             if (pl)
             {
                 Boolean exec = executaProlog();
                 if (exec)
                 {
-                    String resultado = resultadoProlog();
+                    String resultado = resultadoFicheiro(NomeFich);
                     removeFiles();
                     return resultado;
                 }

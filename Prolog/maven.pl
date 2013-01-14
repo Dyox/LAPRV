63b99e556ab95068d83ebf821597ebd1a056dbfc
Dyox <carlosrok2003@gmail.com>
2013-01-14 00:44:02 +0000

    maven.pl completo maven.txt completo
    Signed-off-by: Dyox <carlosrok2003@gmail.com>

--- Prolog/maven.pl
+++ Prolog/maven.pl
@@ -1,7 +1,6 @@
-%listar_amigos_lista(X,Lista):-Lista = [[a,[a,b,c]],[b,[a,b,c]],[d,[a,b,c]]].
 % no(NoID,[TAGS],PosX,PosY)
 no(a,[rock],45,95).
-no(b,[musica,rock],90,95).
+no(b,[rock,musica],90,95).
 no(c,[rock],45,95).
 no(d,[musica],90,95).
 
@@ -23,10 +22,13 @@ ramo(c,b,[namorada],1).
 ramo(b,d,[namorada],1).
 ramo(d,b,[namorada],1).
 
+se_entao_senao(X,Y,Z):-X,!,Y.
+se_entao_senao(X,Y,Z):-Z.
 
 /*elemento numa possição da lista*/
-indexOf([Element|_], Element, 0):- !.
-indexOf([_|Tail], Element, Index):- indexOf(Tail, Element, Index1), !, Index is Index1+1.
+item_at( N, L, Item ) :- item_at( N, 0, L, Item ).   
+item_at( N, Count, [H|_], Item ) :- CountNew is Count + 1,CountNew = N, Item = H.
+item_at( N, Count, [_|T], Item ) :- CountNew is Count + 1,item_at( N, CountNew, T, Item ).
 
 /*maximo de uma lista*/
 max_list([H], H).
@@ -37,12 +39,11 @@ listar_amigos(X,Y):-ramo(X,Y,_,_).
 listar_amigos_lista(X,Lista):-findall(L,listar_amigos(X,L),Lista).
 
 /*procurar amigos com numa dada tag*/
-se_entao_senao(X,Y,Z):-X,!,Y.
-se_entao_senao(X,Y,Z):-Z.
 procurar_tag(Tag,[],[]):-!.
 procurar_tag(Tag,[ID|Resto],Res):-procurar_tag(Tag,Resto,Res1),no(ID,Lista,_,_),se_entao_senao(member(Tag,Lista),append(Res1,[ID],Res),append(Res1,[],Res)),!.
 
 amigos_tag(Tag,[],[]):-!.
-amigos_tag(Tag,[ID|Resto],Res):-amigos_tag(Tag,Resto,Res1),listar_amigos_lista(ID,LA),write(ID),write(LA),nl,procurar_tag(Tag,LA,LAT),length(LAT,T),append(Res1,[T],Res),!.
+amigos_tag(Tag,[ID|Resto],Res):-amigos_tag(Tag,Resto,Res1),listar_amigos_lista(ID,LA),procurar_tag(Tag,LA,LAT),length(LAT,T),append(Res1,[T],Res),!.
 
-maven(Tag,Element):-findall(NoID,no(NoID,_,_,_),L),amigos_tag(Tag,L,Ltam),max_list(Ltam,T),indexOf(Ltam,T,POS),indexOf(L, Element, POS),write(Element).
+maven(Tag,Element):-findall(NoID,no(NoID,_,_,_),L),amigos_tag(Tag,L,Ltam),max_list(Ltam,T),
+			se_entao_senao(T>2,write('Nenhum Maven...'),(item_at(POS,Ltam,T),item_at(POS,L, Element),write(Element))).

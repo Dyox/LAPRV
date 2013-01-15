3df1c5ce75b2a954402fd52358d965fae421f530
Dyox2 <carlosrok2003@hotmail.com>
2013-01-15 16:24:34 +0000

    correcção do caminho mais forte
    plano de testes
    
    Signed-off-by: Dyox2 <carlosrok2003@hotmail.com>

--- Prolog/caminho mais forte.pl
+++ Prolog/caminho mais forte.pl
@@ -1,16 +1,42 @@
-% no(NoID,[listaTags],PosX,PosY)
-no(a,[musica,rock],45,95).
+% grafo
+
+% no(NoID,PosX,PosY)
+no(a,[musica,rock,pimba,zumba],45,95).
 no(b,[musica,rock],90,95).
 no(c,[musica],45,95).
 no(d,[],90,95).
-no(e,[],45,95).
-no(f,['c#'],90,95).
+no(e,[pimba],45,95).
+no(f,['c#',zumba],90,95).
 
 % ramo(No1_ID,No2_ID,Tag,Força)
 ramo(a,b,[namorada],1).
-ramo(a,c,[namorada],8).
-ramo(b,c,[namorada],2).
-
+ramo(b,a,[namorada],1).
+ramo(a,c,[amiga],2).
+ramo(c,a,[amiga],2).
+ramo(a,d,[amiga],1).
+ramo(d,a,[amiga],1).
+ramo(a,e,[amiga],2).
+ramo(e,a,[amiga],2).
+ramo(b,e,[amiga],1).
+ramo(e,b,[amiga],1).
+ramo(d,e,[amiga],1).
+ramo(e,d,[amiga],1).
+ramo(d,f,[amiga,irma],1).
+ramo(f,d,[amiga,irma],1).
+ramo(c,d,[],3).
+ramo(d,c,[],3).
+ramo(e,h,[],3).
+ramo(h,e,[],3).
+ramo(a,z,[],3).
+ramo(z,a,[],3).
+ramo(x,e,[],3).
+ramo(e,x,[],3).
+ramo(x,y,[],3).
+ramo(y,x,[],3).
+ramo(y,w,[],3).
+ramo(w,y,[],3).
+ramo(yy,ww,[],3).
+ramo(ww,yy,[],3).
 
 caminhos(X,Y,P):-todos_caminhos(X,Y,[Y],P).
 todos_caminhos(X,Y,L,[X|L]):-ramo(X,Y,_,_).
@@ -33,8 +59,13 @@ max_list([H|T], M2) :- max_list(T, M), M2 is max(H, M).
 indexOf([Element|_], Element, 0):- !.
 indexOf([_|Tail], Element, Index):- indexOf(Tail, Element, Index1), !, Index is Index1+1.
 
+/*elemento numa possicao da lista*/
+item_at( N, L, Item ) :- item_at( N, 0, L, Item ).   
+item_at( N, Count, [H|_], Item ) :- CountNew is Count + 1,CountNew = N, Item = H.
+item_at( N, Count, [_|T], Item ) :- CountNew is Count + 1,item_at( N, CountNew, T, Item ).
+
 camMaisForte(X,X,Element):-!,write('Nenhum caminho...').
 camMaisForte(X,Y,Element):-findall(P,caminhos(X,Y,P),L),findall(F,forca(X,Y,F),LF),
-			max_list(LF,T),indexOf(LF,T,POS),indexOf(L, Element, POS),write(Element);write('Nenhum caminho...').
+			max_list(LF,T),item_at(POS,LF,T),item_at(POS,L,Element),write(Element);write('Nenhum caminho...').
 
 
